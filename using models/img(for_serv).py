a2970a4f7c82834ea25d1c61bccf7b0fbab13cd3
Sirenko Paul <sirenko.paul@yandex.ru>
2020-03-10 15:30:40 +0700

    clean

--- using models/img(for_serv).py
+++ using models/img(for_serv).py
@@ -9,11 +9,13 @@ import time
 import pickle
 import struct
 import socket
+import asyncio
 
-
+from queue import Queue
 from utils import label_map_util
 from utils import visualization_utils as vis_util
 print("start")
+#________________________INIT_____________________________
 MODEL_NAME = 'inference_graph'
 IMAGE_NAME = 'img'
 # for otnositelnih putey
@@ -43,11 +45,12 @@ num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 print('INIT TF DONE')
 PATH_TO_SAVE = 'output1'
 
-#init serv
+q=Queue()
+
+#_____________________INIT SOCKET-SERV__________________________
 METHOD_RECEPTION = int(input('Reception method(0 - independ client, 1 - webcam): '))
 METHOD_PROCESSING = int(input('Processing method(0 - SQUARE, 1 - SEOQL): '))
 METHOD_SAVING = int(input('SAVING METHOD(0 - on disk, 1 - sender): '))
-
 HOST = ''
 PORT = 9090
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -62,48 +65,46 @@ data = b""
 payload_size = struct.calcsize(">L")
 print("payload_size: {}".format(payload_size))
 NumbFrame = 0
+
+
+#_____________________________DEFS__________________________________
+
 # cycle for recive and processing img
-while True:
+
+async def get_img():
+    while True:
+        start_time = datetime.now()
+        while len(data) < payload_size:
+            data += conn.recv(4096)
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack(">L", packed_msg_size)[0]
+        while len(data) < msg_size:
+            data += conn.recv(1024000)
+        frame_data = data[:msg_size]
+        data = data[msg_size:]
+        # image = np.frombuffer(frame_data, np.uint8)
+        image = pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+    # if you use webcam(black and white - GRAYSKALE)
+        if(METHOD_RECEPTION == 1):
+            image = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
+            q.put(image)
+        print('Time for recive: ', datetime.now() - start_time)
+
+
+
+
+async def processing_IMG():
     start_time = datetime.now()
-    print('start')
-    while len(data) < payload_size:
-        print("Recv: {}".format(len(data)))
-        data += conn.recv(4096)
-    print("Done Recv: {}".format(len(data)))
-    print('time for recive SIZE DATA', datetime.now() - start_time)
-    packed_msg_size = data[:payload_size]
-    data = data[payload_size:]
-    msg_size = struct.unpack(">L", packed_msg_size)[0]
-    print("msg_size: {}".format(msg_size))
-    while len(data) < msg_size:
-        data += conn.recv(1024000)
-    print('time for recive DATA', datetime.now() - start_time)
-
-    ARROW_TIME = datetime.now()
-    frame_data = data[:msg_size]
-    print('frame_data')
-    data = data[msg_size:]
-    print('data load')
-    print('ARROW_TIME: ', ARROW_TIME)
-
-    start_pickle_load_time = datetime.now()
-    # image = np.frombuffer(frame_data, np.uint8)
-    image = pickle.loads(frame_data, fix_imports=True, encoding="bytes")
-    print('time PIKLE LOAD IMG: ', datetime.now() - start_pickle_load_time)
-    print('Image loads')
-    print('time for recive img', datetime.now() - start_time)
-
-# if you use webcam(black and white - GRAYSKALE)
-    if(METHOD_RECEPTION == 1):
-        image = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
-        print('DECODE SUCESFULL')
-
+    while True:
+        if q.empty():
+            continue
+        image = q.get()
 # detect object
-    image_expanded = np.expand_dims(image, axis=0)
-    (boxes, scores, classes, num) = sess.run(
-        [detection_boxes, detection_scores, detection_classes, num_detections],
-        feed_dict={image_tensor: image_expanded})
-
+        image_expanded = np.expand_dims(image, axis=0)
+        (boxes, scores, classes, num) = sess.run(
+            [detection_boxes, detection_scores, detection_classes, num_detections],
+            feed_dict={image_tensor: image_expanded})
 # drawer of square
     if(METHOD_PROCESSING == 0):
         vis_util.visualize_boxes_and_labels_on_image_array(
@@ -115,7 +116,6 @@ while True:
             use_normalized_coordinates=True,
             line_thickness=3,
             min_score_thresh=0.50)
-
 #definer center if cath
     elif (METHOD_PROCESSING == 1):
         scores = np.squeeze(scores)
@@ -134,18 +134,20 @@ while True:
     else:
         print('check METHOD_PROCESSING')
         conn.close()
-
 # saver or sender
 #   cv2.imshow('ImageWindow', image)
     if(METHOD_SAVING == 0):
         cv2.imwrite('{}/{}.png'.format(PATH_TO_SAVE, [datetime.now(), NumbFrame]), image)
     elif (METHOD_SAVING == 1):
-        start_send_time = datetime.now()
         conn.sendall(center_coordinates)
-        print('SEND_TIME: ', datetime.now() - start_send_time)
     else:
         print('check METHOD_SAVING')
         conn.close()
     NumbFrame += 1
     print('Time for one cycle: ', datetime.now() - start_time)
-conn.close()
+
+async def main():
+
+
+
+if __name__ == "__main__":
