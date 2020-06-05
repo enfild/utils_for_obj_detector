2fcf788f552d96f2792385aeb63c7fab8c6ee08a
Paul <paul.sirenko@yandex.ru>
2020-06-05 16:56:05 +0700

    toh

--- prepair/database extender.py
+++ prepair/database extender.py
@@ -4,16 +4,9 @@ import sys
 import time
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 import pathlib
-
 import xml.etree.ElementTree as ET
-import lxml
-from lxml import etree
-from xml.dom import minidom
-
 from datetime import datetime
-from PIL import Image
 
 
 def noisy(noise_typ,image):
@@ -45,44 +38,38 @@ def noisy(noise_typ,image):
         noisy = image + image * gauss
         return noisy
 
+def xml_edit_for_shift(mode, root):
 
-def xml_create(path, mode):
-    xml_list = []
+    width = 1024
+    height = 1024
 
+    for elem in root.iter('width'):
+        width = int(elem.text)
+        print(width)
+
+    for elem in root.iter('height'):
+        height = int(elem.text)
+        print(height)
+
+    # for elem in root.iter('xmin'):
+    #     print(elem.text + "width")
+
+def xml_create(path, mode_name, mode):
     tree = ET.parse(path)
     root = tree.getroot()
     source_file = root.find('filename').text
     print('source_file: ')
     print(source_file)
+    for elem in root.iter('filename'):
+        elem.text = mode + source_file
 
-    output_file = source_file[0:-4] + mode + ".xml"
+    if(mode_name == "flip"):
+        xml_edit_for_shift(mode, root)
+
+    output_file = mode + source_file[0:-4] + ".xml"
+    print("output_file: ")
     print(output_file)
     tree.write(output_file)
-
-    # for member in root.findall('object'):
-    #     value = (root.find('filename').text,
-    #              int(root.find('size')[0].text),
-    #              int(root.find('size')[1].text),
-    #              member[0].text,
-    #              int(member[4][0].text),
-    #              int(member[4][1].text),
-    #              int(member[4][2].text),
-    #              int(member[4][3].text)
-    #              )
-    #     open(PATH_TO_XML)
-
-    #         xml_Doc = etree.parse(PATH_TO_XML)
-    #         root = xml_Doc.getroot()
-
-    #         nodes = xml_Doc.xpath('/annotation/os/item')
-
-
-    #         annotation = etree.Element("annotation")
-    #         filename = str(etree.SubElement(annotation, "filename"))
-    #         path = str(etree.SubElement(annotation, "path"))
-    #         print(filename, path)
-
-
 
 start_time = datetime.now()
 CWD_PATH = ""
@@ -107,38 +94,43 @@ with os.scandir(PATH_TO_IMAGE) as entries:
         
             if mode_name == "proc":  
                 #negativ  
-                neg_image = (255-image)
-                xml_create(PATH_TO_XML, "_neg")
-                cv2.imwrite(os.path.join(PATH_TO_SAVE, "neg_" + entry.name), neg_image)
+                # neg_image = (255-image)
+                # xml_create(PATH_TO_XML, "neg_")
+                # cv2.imwrite(os.path.join(PATH_TO_SAVE, "neg_" + entry.name), neg_image)
 
                 #Brigthness + 30 
                 alpha = 1.0
                 beta = 30
                 image_brig = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
-                cv2.imwrite(os.path.join(PATH_TO_SAVE, "br+30" + entry.name), image_brig)
+                xml_create(PATH_TO_XML, mode_name, "br+30_")
+                cv2.imwrite(os.path.join(PATH_TO_SAVE, "br+30_" + entry.name), image_brig)
 
                 #Brigthness -30 
                 alpha = 1.0
                 beta = -30
                 image_brig = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
-                cv2.imwrite(os.path.join(PATH_TO_SAVE, "br-30" + entry.name), image_brig)
+                xml_create(PATH_TO_XML, mode_name, "br-30_")
+                cv2.imwrite(os.path.join(PATH_TO_SAVE, "br-30_" + entry.name), image_brig)
 
                 #Contrast +0.3
                 alpha = 1.3
                 beta = 0
                 image_cont = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
-                cv2.imwrite(os.path.join(PATH_TO_SAVE, "cont+03" + entry.name), image_cont)
+                xml_create(PATH_TO_XML, mode_name, "cont+03_")
+                cv2.imwrite(os.path.join(PATH_TO_SAVE, "cont+03_" + entry.name), image_cont)
 
                 #Contrast -0.3
                 alpha = 0.7
                 beta = 0
                 image_cont = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
-                cv2.imwrite(os.path.join(PATH_TO_SAVE, "cont-03" + entry.name), image_cont)
+                xml_create(PATH_TO_XML, mode_name, "cont-03_")
+                cv2.imwrite(os.path.join(PATH_TO_SAVE, "cont-03_" + entry.name), image_cont)
     
                 #subpixel shift
                 row, col, channel = image.shape
                 M = np.float32([[1,0,0.5],[0,1,0.5]])
                 image_shift = cv2.warpAffine(image, M, (col, row), flags=cv2.INTER_LINEAR, borderMode=cv2.BORDER_REFLECT_101)
+                xml_create(PATH_TO_XML, mode_name, "shift_")
                 cv2.imwrite(os.path.join(PATH_TO_SAVE, "shift_" + entry.name), image_shift)
     
                 # elif proc_name == "conturs":
@@ -150,13 +142,16 @@ with os.scandir(PATH_TO_IMAGE) as entries:
     
             elif mode_name == "flip":
                 image_ver = cv2.flip(image, 0)
-                cv2.imwrite(os.path.join(PATH_TO_SAVE, entry.name), image_ver)
+                xml_create(PATH_TO_XML, mode_name, "ver_")
+                cv2.imwrite(os.path.join(PATH_TO_SAVE, "ver_" + entry.name), image_ver)
     
                 image_hor = cv2.flip(image, 1)
-                cv2.imwrite(os.path.join(PATH_TO_SAVE, entry.name), image_hor)
+                xml_create(PATH_TO_XML, mode_name, "hor_")
+                cv2.imwrite(os.path.join(PATH_TO_SAVE, "hor_" + entry.name), image_hor)
     
                 image_bot = cv2.flip(image, -1)
-                cv2.imwrite(os.path.join(PATH_TO_SAVE, entry.name), image_bot)
+                xml_create(PATH_TO_XML, mode_name, "bot_")
+                cv2.imwrite(os.path.join(PATH_TO_SAVE, "bot_" + entry.name), image_bot)
     
             else:
                 print("make your choice")
